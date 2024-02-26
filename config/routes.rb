ce5c8b9b73450135bb2c6f096edfa16ee2a00112
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-26 08:47:09 -0300

    Add index action to transacoes controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :clientes, only: [:index, :show] do
-    resources :transacoes, only: [:create]
+    resources :transacoes, only: [:create, :index]
   end
 end
