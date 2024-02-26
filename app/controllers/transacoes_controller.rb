6baa4fe8901c3782789ff37d1f5d6d58691c3ce9
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-26 08:49:15 -0300

    Add TransacoesController with index and create actions

---
+++ app/controllers/transacoes_controller.rb
@@ -0,0 +1,26 @@
+class TransacoesController < ApplicationController
+  before_action :set_cliente
+
+  def index
+    render json: cliente_transacoes, status: :ok
+  end
+
+  def create
+    transacao = Transacoes::Create.new(params[:cliente_id], transacao_params).call
+    render json: { limite: @cliente.reload.limite, saldo: transacao.handle_saldo }, status: :ok
+  end
+
+  private
+
+  def set_cliente
+    @cliente = Cliente.find(params[:cliente_id])
+  end
+
+  def transacao_params
+    params.permit(:valor, :tipo, :descricao)
+  end
+
+  def cliente_transacoes
+    Transacao.where(cliente_id: params[:cliente_id])
+  end
+end
