33952f3dceef4663290d432f1c3aa125a1399430
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-26 21:16:01 -0300

    Update schema.rb with saldo column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_222936) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_235528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_222936) do
     t.integer "limite"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "saldo"
   end
 
   create_table "transacoes", force: :cascade do |t|
