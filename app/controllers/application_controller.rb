ec280528e697d366ab5a52c35a4c4ae30636624d
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-27 19:21:08 -0300

    Add rescue_from for ActiveRecord::RecordInvalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_not_permitted
 
   private
 
   def record_not_found
     render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def record_not_permitted
+    render json: { error: 'Record not permitted' }, status: :unprocessable_entity
   end
 end
