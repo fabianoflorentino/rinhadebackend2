e366f255d0044f8dcfb9e7dacb91e86a64533b85
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-27 21:21:54 -0300

    Add Transacoes::Extrato use case

---
+++ app/use_cases/transacoes/extrato.rb
@@ -0,0 +1,38 @@
+module Transacoes
+  class Extrato
+    def initialize(cliente_id)
+      @cliente_id = cliente_id
+    end
+
+    def call
+      extrato
+    end
+
+    private
+
+    def cliente
+      Cliente.find(@cliente_id)
+    end
+
+    def transacoes
+      Transacao.where(cliente_id: @cliente_id)
+    end
+
+    def extrato
+      {
+        saldo: {
+          total: cliente.saldo,
+          data_extrato: Time.zone.now,
+          limite: cliente.limite
+        },
+        ultimas_transacoes: transacoes.map do |transacao|
+          {
+            valor: transacao.valor,
+            tipo: transacao.tipo,
+            data: transacao.created_at
+          }
+        end
+      }
+    end
+  end
+end
