8f94456942b63838cefad0020dee039e6083f1f1
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-28 09:22:33 -0300

    Add "rinhadebackend2" to development.rb hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "rinhadebackend2"
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
 
