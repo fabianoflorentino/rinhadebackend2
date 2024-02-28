b5df3534015dd8c9029a36469cf9bb90746e92b0
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-28 17:35:46 -0300

    Enable caching in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  config.action_controller.perform_caching = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
