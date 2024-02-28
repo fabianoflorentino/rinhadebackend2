9da1c500d083c567b614e68647e7fc009a131849
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-28 20:25:42 -0300

    Refactor extrato method in Transacoes module

--- app/use_cases/transacoes/extrato.rb
+++ app/use_cases/transacoes/extrato.rb
@@ -10,29 +10,30 @@ module Transacoes
 
     private
 
+    def transacoes
+      Transacao.includes(:cliente).where(cliente_id: @cliente_id).order(created_at: :desc)
+    end
+
     def cliente
       Cliente.find_by_id(@cliente_id)
     end
 
-    def transacoes
-      Transacao.where(cliente_id: @cliente_id)
-    end
-
     def extrato
-      {
-        saldo: {
-          total: cliente.saldo,
-          data_extrato: Time.zone.now,
-          limite: cliente.limite
-        },
-        ultimas_transacoes: transacoes.map do |transacao|
-          {
-            valor: transacao.valor,
-            tipo: transacao.tipo,
-            data: transacao.created_at
-          }
-        end
+      saldo ={
+        total: cliente.saldo,
+        data_extrato: Time.zone.now,
+        limite: cliente.limite
       }
+
+      ultimas_transacoes = transacoes.last(10).map do |transacao|
+        {
+          valor: transacao.valor,
+          tipo: transacao.tipo,
+          data: transacao.created_at
+        }
+      end
+
+      { saldo: saldo, ultimas_transacoes: ultimas_transacoes}
     end
   end
 end
