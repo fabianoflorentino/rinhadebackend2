43945d06cc22867f5ba27eb7ed0848cf008bdc0a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-28 21:59:22 -0300

    Refactor transacoes_controller.rb to improve code readability and performance

--- app/controllers/transacoes_controller.rb
+++ app/controllers/transacoes_controller.rb
@@ -1,26 +1,25 @@
 class TransacoesController < ApplicationController
-  before_action :set_cliente
-
   def index
-    render json: cliente_transacoes, status: :ok
+    render json: transacoes, status: :ok
   end
 
   def create
     Transacoes::Create.new(params[:cliente_id], transacao_params).call
-    render json: { limite: @cliente.limite, saldo: @cliente.reload.saldo }, status: :ok
+    render json: { limite: cliente.limite, saldo: cliente.reload.saldo }, status: :ok
   end
 
   private
 
-  def set_cliente
-    @cliente = Cliente.find_by_id(params[:cliente_id])
+  def cliente
+    Cliente.find_by_id(params[:cliente_id])
   end
 
   def transacao_params
     params.permit(:valor, :tipo, :descricao)
   end
 
-  def cliente_transacoes
-    Transacao.where(cliente_id: params[:cliente_id])
+  def transacoes
+    transacoes = Transacao.includes(:cliente).where(cliente_id: params[:cliente_id])
+    transacoes.order(created_at: :desc).limit(10)
   end
 end
