bb59d879c310e26b3fa60cd060c8074b48afb8b7
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-28 21:59:11 -0300

    Refactor transacoes method in extrato.rb to improve readability and performance

--- app/use_cases/transacoes/extrato.rb
+++ app/use_cases/transacoes/extrato.rb
@@ -11,7 +11,8 @@ module Transacoes
     private
 
     def transacoes
-      Transacao.includes(:cliente).where(cliente_id: @cliente_id).order(created_at: :desc)
+      transacoes = Transacao.includes(:cliente).where(cliente_id: @cliente_id)
+      transacoes.order(created_at: :desc).limit(10)
     end
 
     def cliente
@@ -19,21 +20,20 @@ module Transacoes
     end
 
     def extrato
-      saldo ={
-        total: cliente.saldo,
-        data_extrato: Time.zone.now,
-        limite: cliente.limite
+      {
+        saldo: {
+          total: cliente.saldo,
+          data_extrato: Time.zone.now,
+          limite: cliente.limite
+        },
+        ultimas_transacoes: transacoes.last(10).map do |transacao|
+          {
+            valor: transacao.valor,
+            tipo: transacao.tipo,
+            data: transacao.created_at
+          }
+        end
       }
-
-      ultimas_transacoes = transacoes.last(10).map do |transacao|
-        {
-          valor: transacao.valor,
-          tipo: transacao.tipo,
-          data: transacao.created_at
-        }
-      end
-
-      { saldo: saldo, ultimas_transacoes: ultimas_transacoes}
     end
   end
 end
