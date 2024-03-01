fa94488fada8d5e361ffca3aba7253fa02c328d3
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-02-29 22:21:38 -0300

    Add rescue_from for ParseError in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_not_permitted
+  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :handle_bad_request
 
   private
 
@@ -10,5 +11,10 @@ class ApplicationController < ActionController::API
 
   def record_not_permitted
     render json: { error: 'Record not permitted' }, status: :unprocessable_entity
+  end
+
+  def handle_bad_request(exception)
+    Rails.logger.error "Erro 400: #{exception.message}"
+    render json: { error: "#{exception.message}" }, status: :bad_request
   end
 end
